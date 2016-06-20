be3311c2604297f747c577ffc427770ae1e7c18f
Jorma Rolf Bork <jbork@ads.mobilcom.de>
2016-06-20 11:24:51 +0200

    erste Versuche der Kollisionsabfrage

--- src/bombermanGui/BombermanBorkKnebel.java
+++ src/bombermanGui/BombermanBorkKnebel.java
@@ -1,194 +1,196 @@
 package bombermanGui;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Graphics;
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
 import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Vector;
+
 import javax.imageio.ImageIO;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import javax.swing.JButton;
 import javax.swing.SwingConstants;
 import javax.swing.Timer;
 
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-
 public class BombermanBorkKnebel {
-	
-	public static bombermanGui pBombermanGui;
-	
-	public static class bombermanGui extends JPanel {
 
-//		COM
-		
+	public static bombermanGui pBombermanGui;
+
+	public static class bombermanGui extends JPanel implements KeyEventDispatcher {
+
+		// COM
+
 		private static final long serialVersionUID = 1L;
 		Point playerPosition = new Point(25, 25);
-		ArrayList<Point> wallPositionList;
+		static ArrayList<Point> wallPositionList = new ArrayList<>();
 		Point tntPOS = new Point(6, 6);
-//		Point doorPosition = new Point(0, 5);
+		// Point doorPosition = new Point(0, 5);
 		Point[] snakePositions = { new Point(30, 2), null, null, null, null };
 		Point wallPosition = new Point();
-		BufferedImage wallImage, woodImage, tntImage;
+		BufferedImage wallImage, woodImage, tntImage, background;
 		BufferedImage imageDestroyable;
 		Player player1, player2;
 		boolean rich = false;
 		boolean end = false;
-		public final int WIDTH = 775, HEIGHT = 375;
+		final public static int WIDTH = 775;
+		final public static int HEIGHT = 375;
+		private Vector<Integer> keysPressed = new Vector<>(20);
+		int counter = 0;
 
 		bombermanGui() {
 			setPreferredSize(new Dimension(WIDTH, HEIGHT));
 			setFocusable(true);
-			
-			player1 = new Player("player1", 25, 25, "img/creeper.png", 1);
+			fillWallPositionList();
+
+			player1 = new Player("player1", 25, 25, "img/creeper.png", 1, keysPressed);
 			player1.start();
-			player2 = new Player("player2", 75, 75, "img/creeper.png", 2);
+			player2 = new Player("player2", 75, 75, "img/creeper.png", 2, keysPressed);
 			player2.start();
 
-			addKeyListener(player1);
-			addKeyListener(player2);
-			
+			KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(this);
+
 			try {
 				wallImage = ImageIO.read(new File("img/wall.png"));
 				woodImage = ImageIO.read(new File("img/wood.png"));
 				tntImage = ImageIO.read(new File("img/tnt.png"));
 
-//				creeperImage = ImageIO.read(BombermanBorkKnebel.class.getResourceAsStream("creeper.png"));
+				// creeperImage =
+				// ImageIO.read(BombermanBorkKnebel.class.getResourceAsStream("creeper.png"));
 			} catch (IOException e) {
 				e.printStackTrace();
+			}
+			background = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+			background.getGraphics().setColor(Color.GRAY);
+			background.getGraphics().fillRect(0, 0, WIDTH, HEIGHT);
+			for (Point position : wallPositionList) {
+				background.getGraphics().drawImage(wallImage, position.x, position.y, null);
 			}
 		}
 
 		@Override
 		protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
-			
+			g.drawImage(background, 0, 0, null);
+			// Figuren zeichnen
 			g.drawImage(player1.getImg(), player1.getX(), player1.getY(), 20, 20, null);
 			g.drawImage(player2.getImg(), player2.getX(), player2.getY(), 20, 20, null);
 
-			// Figuren zeichnen
 			g.drawImage(woodImage, 50, 25, 25, 25, null);
 			g.drawImage(tntImage, 50, 75, 25, 25, null);
 
 			// Die Wände drumherum
+			// for (int y = 0; y < HEIGHT; y += 25) {
+			// g.drawImage(wallImage, 0, y, null);
+			// g.drawImage(wallImage, WIDTH - 25, y, null);
+			// }
+			// for (int x = 0; x < WIDTH; x += 25) {
+			// g.drawImage(wallImage, x, 0, null);
+			// g.drawImage(wallImage, x, HEIGHT - 25, null);
+			// }
+			// // Die Wände innerhalb des Spielfeldes
+			// for (int y = 0; y < HEIGHT; y += 50) {
+			// for (int x = 0; x < WIDTH; x += 50) {
+			// g.drawImage(wallImage, x, y, null);
+			// }
+			// }
+			if (counter == 0) {
+				for (Point position : wallPositionList) {
+					g.drawImage(wallImage, position.x, position.y, null);
+				}
+			}
+			counter++;
+		}
+
+		public void fillWallPositionList() {
 			for (int y = 0; y < HEIGHT; y += 25) {
-				g.drawImage(wallImage, 0, y, null);
-				g.drawImage(wallImage, WIDTH - 25, y, null);
-//				wallPositionList.add(new Point(0, y));
-//				wallPositionList.add(new Point(WIDTH - 25, y));
+				wallPositionList.add(new Point(0, y));
+				wallPositionList.add(new Point(WIDTH - 25, y));
 			}
 			for (int x = 0; x < WIDTH; x += 25) {
-				g.drawImage(wallImage, x, 0, null);
-				g.drawImage(wallImage, x, HEIGHT - 25, null);
-//				wallPositionList.add(new Point(x, 0));
-//				wallPositionList.add(new Point(x, HEIGHT - 25));
+				wallPositionList.add(new Point(x, 0));
+				wallPositionList.add(new Point(x, HEIGHT - 25));
 			}
 			// Die Wände innerhalb des Spielfeldes
 			for (int y = 0; y < HEIGHT; y += 50) {
 				for (int x = 0; x < WIDTH; x += 50) {
-					g.drawImage(wallImage, x, y, null);
-//					wallPositionList.add(new Point(x, y));
+					wallPositionList.add(new Point(x, y));
 				}
-			}
-			for (int y = 0; y < HEIGHT; y++) {
-				for (int x = 0; x < WIDTH; x++) {
-					char c = ' ';
-					Point p = new Point(x, y);
-
-					if (tntPOS.equals(p))
-						c = '$';
-
-					// g.hitclip für collisionsabfrage möglich?
-					if (g.hitClip(50, 125, 25, 25))
-
-						if (Arrays.asList(snakePositions).contains(p))
-							c = 'S';
-					if (!Character.isWhitespace(c))
-						g.drawString(Character.toString(c), x * 10, y * 10);
-				}
-			}
-			// Status aktualisieren
-//			if (rich && playerPosition.equals(doorPosition)) {
-//				System.out.println("Gewonnen!");
-//				end = true;
-//				return;
-//			}
-//			if (Arrays.asList(snakePositions).contains(playerPosition)) {
-//				System.out.println("ZZZZZZZ. Die Schlage hat dich!");
-//				end = true;
-//				return;
-//			}
-			if (playerPosition.equals(tntPOS)) {
-				rich = true;
-				tntPOS.setLocation(-1, -1);
 			}
 		}
 
-//		@Override
-//		public void keyPressed(KeyEvent e) {
-//			if (end)
-//				return;
-//			// Tasteneingabe und Spielerposition verÃ¤ndern
-//			switch (e.getKeyCode()) {
-//			case KeyEvent.VK_UP:
-//				player1.setY ( Math.max(0, player1.getY()- travelPixel) );
-//				break;
-//			case KeyEvent.VK_DOWN:
-//				player1.setY ( Math.min(HEIGHT - travelPixel, player1.getY() + travelPixel) );
-//				break;
-//			case KeyEvent.VK_LEFT:
-//				player1.setX ( Math.max(0, player1.getX() - travelPixel) );
-//				break;
-//			case KeyEvent.VK_RIGHT:
-//				player1.setX ( Math.min(WIDTH - travelPixel, player1.getX() + travelPixel) );
-//				break;
-//			case KeyEvent.VK_W:
-//				player2.setY ( Math.max(0, player2.getY()- travelPixel) );
-//				break;
-//			case KeyEvent.VK_A:
-//				player2.setY ( Math.min(HEIGHT - travelPixel, player2.getY() + travelPixel) );
-//				break;
-//			case KeyEvent.VK_S:
-//				player2.setX ( Math.max(0, player2.getX() - travelPixel) );
-//				break;
-//			case KeyEvent.VK_D:
-//				player2.setX ( Math.min(WIDTH - travelPixel, player2.getX() + travelPixel) );
-//				break;
-//			}
-//		}
-//
-//		@Override
-//		public void keyTyped(KeyEvent e) {
-//			// TODO Auto-generated method stub
-//			
-//		}
-//
-//		@Override
-//		public void keyReleased(KeyEvent e) {
-//			// TODO Auto-generated method stub
-//			
-//		}
+		@Override
+		public boolean dispatchKeyEvent(KeyEvent e) {
+			// Im Gegensatz zu KeyListener gibt es beim KeyEventDispatcher nur
+			// eine Methode fr
+			// alle KeyEvents, daher mssen wir den Typ des Events selbst
+			// unterscheiden
+			if (e.getID() == KeyEvent.KEY_PRESSED) {
+				// Taste in die Liste der aktuell gedrckten Tasten schreiben,
+				// wenn sie noch nicht darin vorhanden ist (das Event
+				// kann mehrmals whrend des gerckthalten ausgelst werden).
+				if (!keysPressed.contains(e.getKeyCode())) {
+					keysPressed.add(e.getKeyCode());
+				}
+			} else if (e.getID() == KeyEvent.KEY_RELEASED) {
+				// Taste wieder aus der Liste entfernen; manuelles boxen,
+				// damit die richtige remove-Funktion aufgerufen wird.
+				keysPressed.remove((Integer) e.getKeyCode());
+			}
+
+			return true; // nichts weiter mit dem KeyEvent machen
+		}
+
+		// @Override
+		// public void keyTyped(KeyEvent e) {
+		// // TODO Auto-generated method stub
+		//
+		// }
+		//
+		// @Override
+		// public void keyReleased(KeyEvent e) {
+		// // TODO Auto-generated method stub
+		//
+		// }
 
 	}
 
 	public static void main(String[] args) {
 		JFrame f = new JFrame("BOMBERMAN");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// f.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE); //Statt
+		// EXIT_ON_CLOSE unseren eigenen WindowListener verwenden
+		// f.addWindowListener(new WindowAdapter() {
+		// @Override
+		// public void windowClosing(WindowEvent e) {
+		// KeyboardFocusManager.getCurrentKeyboardFocusManager().removeKeyEventDispatcher(pBombermanGui);
+		// //KeyStrokes2.this.dispose();
+		// System.exit(0);
+		// }
+		// });
+
 		pBombermanGui = new bombermanGui();
 		f.getContentPane().add(pBombermanGui);
-		
+
 		JPanel controlPanel = new JPanel();
 		FlowLayout flowLayout = (FlowLayout) controlPanel.getLayout();
 		f.getContentPane().add(controlPanel, BorderLayout.SOUTH);
-		
+
 		JButton btnStartGame = new JButton("Start Game");
 		btnStartGame.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -198,14 +200,16 @@ public class BombermanBorkKnebel {
 		controlPanel.add(btnStartGame);
 		f.pack();
 		f.setVisible(true);
-		
-		 Timer timer = new Timer (1, new ActionListener() {
-		 @Override
-		 public void actionPerformed(ActionEvent e){
-			 pBombermanGui.repaint();
-		 }
-		 });
-		 timer.start();		
-		
+
+		Timer timer = new Timer(1, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				pBombermanGui.player1.update();
+				pBombermanGui.player2.update();
+				pBombermanGui.repaint();
+			}
+		});
+		timer.start();
+
 	}
 }
--- src/bombermanGui/Player.java
+++ src/bombermanGui/Player.java
@@ -2,21 +2,28 @@ package bombermanGui;
 
 import java.awt.Graphics;
 import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
+import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.WindowAdapter;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Vector;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JComponent;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class Player extends Thread implements KeyListener,KeyEventDispatcher {
-	
+import bombermanGui.BombermanBorkKnebel.bombermanGui;
+
+public class Player extends Thread {
+
 	private String name;
-	private int x,y;
+	private int x, y;
 	private String img;
 	private BufferedImage playerImage;
 	private final boolean destroyable = true;
@@ -24,50 +31,50 @@ public class Player extends Thread implements KeyListener,KeyEventDispatcher {
 	private Graphics g;
 	private ImageIcon playerIcon;
 	boolean end = false;
-	int stepSize = 5;
+	int stepSize = 1;
 	int whichPlayer;
-//	private String playerImage="";
-	
-	
-	public Player(String name, int x,int y, String img, int whichPlayer) {
+	Vector<Integer> keysPressed;
+
+	public Player(String name, int x, int y, String img, int whichPlayer, Vector<Integer> keysPressed) {
 		super();
 		this.name = name;
 		this.y = y;
 		this.x = x;
-		
+
 		this.img = img;
 		this.whichPlayer = whichPlayer;
-		
+		this.keysPressed = keysPressed;
+
 		try {
 			playerImage = ImageIO.read(new File(img));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-            //playerIcon = new ImageIcon(img);
-			//this.playerLabel = new JLabel();
-			//playerLabel.paintComponents(g);
-			//playerLabel.setIcon(playerIcon);
+
 	}
-	
+
 	@Override
 	public void run() {
-		
+
 	}
-	
-	public BufferedImage getImg(){
+
+	public BufferedImage getImg() {
 		return playerImage;
 	}
-	
+
 	public int getX() {
 		return x;
 	}
+
 	public void setX(int x) {
 		this.x = x;
 	}
+
 	public int getY() {
 		return y;
 	}
+
 	public void setY(int y) {
 		this.y = y;
 	}
@@ -83,73 +90,68 @@ public class Player extends Thread implements KeyListener,KeyEventDispatcher {
 	public JLabel getPlayerLabel() {
 		return playerLabel;
 	}
+
 	public void setPlayerLabel(JLabel playerLabel) {
 		this.playerLabel = playerLabel;
 	}
+
 	public Graphics getG() {
 		return g;
 	}
+
 	public void setG(Graphics g) {
 		this.g = g;
 	}
+
 	public boolean isDestroyable() {
 		return destroyable;
 	}
-	
-	@Override
-	public void keyPressed(KeyEvent e) {
 
-		if(this.whichPlayer ==1){
-		// Tasteneingabe und Spielerposition verÃ¤ndern
-		switch (e.getKeyCode()) {
-		case KeyEvent.VK_UP:
-			this.setY ( Math.max(0, this.getY()- stepSize) );
-			break;
-		case KeyEvent.VK_DOWN:
-			this.setY ( Math.min(BombermanBorkKnebel.pBombermanGui.WIDTH - stepSize, this.getY() + stepSize) );
-			break;
-		case KeyEvent.VK_LEFT:
-			this.setX ( Math.max(0, this.getX() - stepSize) );
-			break;
-		case KeyEvent.VK_RIGHT:
-			this.setX ( Math.min(BombermanBorkKnebel.pBombermanGui.WIDTH - stepSize, this.getX() + stepSize) );
-			break;
-		}
-		}
-		else if(this.whichPlayer == 2){
-			switch (e.getKeyCode()) {
-			case KeyEvent.VK_W:
-				this.setY ( Math.max(0, this.getY()- stepSize) );
-				break;
-			case KeyEvent.VK_S:
-				this.setY ( Math.min(BombermanBorkKnebel.pBombermanGui.WIDTH - stepSize, this.getY() + stepSize) );
-				break;
-			case KeyEvent.VK_A:
-				this.setX ( Math.max(0, this.getX() - stepSize) );
-				break;
-			case KeyEvent.VK_D:
-				this.setX ( Math.min(BombermanBorkKnebel.pBombermanGui.WIDTH - stepSize, this.getX() + stepSize) );
-				break;
-			}			
-		}
-	}
+	/*
+	 * Die Update-Funktion lässt den Spielstand um eine bestimmte Zeitspanne
+	 * (period) voranschreiten, wobei Spielregeln/-logik implementiert und
+	 * Tastatureingaben verarbeitet werden.
+	 */
+	public void update() {
 
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+		for (Point point : bombermanGui.wallPositionList)
+			if ((this.x >= (25)) && (this.x <= bombermanGui.WIDTH) && (this.y >= (25)) && (this.y <= bombermanGui.HEIGHT)) {
+				// über alle gedrückten Tasten iterieren
+				if (this.whichPlayer == 1) {
+					for (int key : keysPressed) {
+						if (key == KeyEvent.VK_UP) {
+							this.setY(Math.max(0, this.getY() - stepSize));
+						}
+						if (key == KeyEvent.VK_DOWN) {
+							this.setY(Math.min(BombermanBorkKnebel.pBombermanGui.WIDTH - stepSize,
+									this.getY() + stepSize));
+						}
+						if (key == KeyEvent.VK_LEFT) {
+							this.setX(Math.max(0, this.getX() - stepSize));
+						}
+						if (key == KeyEvent.VK_RIGHT) {
+							this.setX(Math.min(BombermanBorkKnebel.pBombermanGui.WIDTH - stepSize,
+									this.getX() + stepSize));
+						}
+					}
+				} else if (this.whichPlayer == 2) {
+					for (int key : keysPressed) {
+						if (key == KeyEvent.VK_W) {
+							this.setY(Math.max(0, this.getY() - stepSize));
+						}
+						if (key == KeyEvent.VK_S) {
+							this.setY(Math.min(BombermanBorkKnebel.pBombermanGui.WIDTH - stepSize,
+									this.getY() + stepSize));
+						}
+						if (key == KeyEvent.VK_A) {
+							this.setX(Math.max(0, this.getX() - stepSize));
+						}
+						if (key == KeyEvent.VK_D) {
+							this.setX(Math.min(BombermanBorkKnebel.pBombermanGui.WIDTH - stepSize,
+									this.getX() + stepSize));
+						}
+					}
+				}
+			}
 	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public boolean dispatchKeyEvent(KeyEvent e) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	
 }
